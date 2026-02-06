a2003ecdf220c49a36bd172ab6cd650a1db0eb74
luckyy071982 <lucky.y.071982@gmail.com>
2026-02-06 21:30:31 +0700

    initial layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import { MusicPlayer } from "@/components/MusicPlayer";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-50 p-16">
-      <MusicPlayer />
+    <div className="flex min-h-screen items-center justify-center bg-neutral-950 p-16">
+      <div className=" w-125 h-87.5 bg-[#0F0F0F] rounded-2xl shadow-lg shadow-black/25 p-4 border">
+        <MusicPlayer />
+      </div>
     </div>
   );
 }
--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -6,9 +6,9 @@
 
 export function MusicPlayer() {
   // TODO: Implementasikan state management untuk playing, paused, loading
-  
+
   // TODO: Implementasikan handler untuk play/pause
-  
+
   // TODO: Implementasikan komponen music player sesuai desain Figma
   // Struktur yang perlu dibuat:
   // - Container dengan background dan shadow animations
@@ -16,13 +16,19 @@ export function MusicPlayer() {
   // - Equalizer bars dengan stagger effect
   // - Progress bar dengan fill animation
   // - Control buttons (play/pause, skip, volume)
-  
+
   return (
-    <div className="w-full max-w-md">
-      {/* TODO: Implementasikan music player di sini */}
-      <p className="text-center text-gray-500">
-        Mulai implementasi music player di sini - Siap
-      </p>
+    <div className="w-full h-full border-white border flex flex-col gap-4">
+      <div className="w-full border-white border h-35.5">
+        {/* TODO: Implementasikan music player di sini */}
+        <p className="text-center text-gray-500">
+          Mulai implementasi music player di sini - Siap
+        </p>
+      </div>
+      <div className="h-2 border border-white">progress bar</div>
+      <div className="h-6 border border-white">time start time end</div>
+      <div className="h-14 border border-white">buttons</div>
+      <div className="h-4 border border-white">volume</div>
     </div>
   );
 }
