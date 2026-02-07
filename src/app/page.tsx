715c242f562a1a20a440ca9862b422f522485017
luckyy071982 <lucky.y.071982@gmail.com>
2026-02-07 08:21:54 +0700

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import { MusicPlayer } from "@/components/MusicPlayer";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-neutral-950 p-16">
-      <div className=" w-125 h-87.5 bg-[#0F0F0F] rounded-2xl shadow-lg shadow-black/25 p-4 border">
-        <MusicPlayer />
-      </div>
+      <MusicPlayer />
     </div>
   );
 }
